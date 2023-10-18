added tracking the number of Unique k-points for each calculation, and fixed plots with correct number of unique k-points

lab2_samples/problem2/Ge_script.py
@@ -70,8 +70,9 @@ def lattice_scan(nk = 3, ecut = 30, alat = 5.0):
     output = compute_energy(alat=alat, ecut=ecut, nk=nk)
     print(output)
     energy = output['energy']/13.6057039763/2 # convert from Rydberg to eV/atom (2 atoms per primitive cell)
-    
-    return energy
+    n_unique_k_points = output['n_unique_k_points']
+
+    return energy, n_unique_k_points
 
 
 if __name__ == '__main__':
@@ -81,14 +82,16 @@ if __name__ == '__main__':
 
     energies, times = [], []
     nks = [int(x) for x in np.arange(1,10)]
+    nks_unique = []
     for nk in nks:
         start = time.time()
-        e = lattice_scan(nk = nk)
+        e, nk_unique = lattice_scan(nk = nk)
         print(nk, e)
         end = time.time()
 
         energies.append(e)
         times.append(end-start)
+        nks_unique.append(nk_unique)
     
     print('energies', energies)
     print('times', times)
@@ -96,14 +99,14 @@ if __name__ == '__main__':
     nks = np.array(nks)**3
 
     plt.figure()
-    plt.scatter(nks, energies)
+    plt.scatter(nks_unique, energies)
     plt.xlabel('Number of k-points')
     plt.ylabel('Energy (eV)')
     plt.savefig('2A.png', dpi = 100)
     plt.show()
 
     plt.figure()
-    plt.scatter(nks, times)
+    plt.scatter(nks_unique, times)
     plt.xlabel('Number of k-points')
     plt.ylabel('Wall time (s)')
     plt.savefig('2B.png', dpi = 100)

lab2_samples/problem4/Ge_script.py
@@ -77,8 +77,9 @@ def lattice_scan(nk = 4, ecut = 30, alat = 5.0):
     print(output)
     force = output['force']
     # *25.71104309541616 # convert from Ryd/Bohr to eV/Angstrom
-    
-    return force
+    n_unique_k_points = output['n_unique_k_points']
+
+    return force, n_unique_k_points
 
 
 if __name__ == '__main__':
@@ -88,14 +89,16 @@ if __name__ == '__main__':
 
     forces, times = [], []
     nks = [int(x) for x in np.arange(1,10)]
+    nks_unique = []
     for nk in nks:
         start = time.time()
-        f = lattice_scan(nk = nk)
+        f, nk_unique  = lattice_scan(nk = nk)
         print(nk, f)
         end = time.time()
 
         forces.append(f)
         times.append(end-start)
+        nks_unique.append(nk_unique)
     
     print('forces', forces)
     print('times', times)
@@ -103,17 +106,10 @@ if __name__ == '__main__':
     nks = np.array(nks)**3
 
     plt.figure()
-    plt.scatter(nks, forces)
+    plt.scatter(nks_unique, forces)
     plt.xlabel('Number of k-points')
     plt.ylabel('Force (eV/Angstrom)')
     plt.savefig('4.png', dpi = 100)
     plt.show()
-
-    # plt.figure()
-    # plt.scatter(nks, times)
-    # plt.xlabel('Number of k-points')
-    # plt.ylabel('Wall time (s)')
-    # plt.savefig('2B.png', dpi = 100)
-    # plt.show()
 
     